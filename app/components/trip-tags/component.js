b4f69077328c9587dd3ee4cf48f0574ece8e2cdf
erinsul <erin_sullivan50@yahoo.com>
2016-08-08 10:48:58 -0400

    Remove console log

--- app/components/trip-tags/component.js
+++ app/components/trip-tags/component.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     addDescription(data) {
-      console.log('up a level');
       data.trip = this.get('trip');
       this.sendAction('addDescription', data);
     }
